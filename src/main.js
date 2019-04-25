4626ed622595b537e5abdd366b21dac2a63f6d1d
keshanju <keshanju@leigod.com>
2019-04-25 15:30:13 +0800

    update

---
+++ src/main.js
@@ -0,0 +1,32 @@
+// 导入所需模块
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import vuex from 'vuex'
+// ajax
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+// ui 框架
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 懒加载
+import { Lazyload } from 'vant'
+// 瀑布流
+import { Waterfall } from 'vant'
+import store from './store/index'
+Vue.use(Waterfall)
+Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
+// 状态管理
+Vue.use(vuex)
+Vue.use(Vant)
+Vue.use(Lazyload)
+
+/* VUE实例 */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: {App},
+  template: '<App/>'
+})
